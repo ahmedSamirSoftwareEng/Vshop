2c2e2a9214fe08858dbcf21aa45bd4088fe15478
ahmedSamirSoftwareEng <ahmedsamirsoftwareengineer@gmail.com>
2025-04-14 10:48:47 +0200

    split adminLayout

---
+++ resources/js/pages/admin/components/AdminLayout.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="bg-gray-50 antialiased dark:bg-gray-900">
+        <!-- Navbar start -->
+        <Navbar />
+        <!-- Navbar end-->
+
+        <!-- Sidebar start -->
+        <Sidebar />
+        <!-- Sidebar end -->
+
+        <slot />
+    </div>
+</template>
+
+<script setup>
+import { initFlowbite } from 'flowbite';
+import { onMounted } from 'vue';
+import Navbar from './Navbar.vue';
+import Sidebar from './Sidebar.vue';
+
+// initialize components based on data attribute selectors
+onMounted(() => {
+    initFlowbite();
+});
+</script>
